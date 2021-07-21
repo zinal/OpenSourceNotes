4bdaf8473a3bad29305c066ccb24876a81100876
Maksim Zinal <mzinal@ru.ibm.com>
2021-07-21 11:37:02 +0300

    дополнительные базы данных, не упомянутые в руководстве по установке CDP

--- cloudera-install/cloudera-roles.sql
+++ cloudera-install/cloudera-roles.sql
@@ -6,6 +6,9 @@ CREATE ROLE oozie LOGIN PASSWORD 'P@ssw0rd';
 CREATE ROLE das LOGIN PASSWORD 'P@ssw0rd';
 CREATE ROLE schemaregistry LOGIN PASSWORD 'P@ssw0rd';
 CREATE ROLE smm LOGIN PASSWORD 'P@ssw0rd';
+CREATE ROLE actmon LOGIN PASSWORD 'P@ssw0rd';
+CREATE ROLE navaud LOGIN PASSWORD 'P@ssw0rd';
+CREATE ROLE navmeta LOGIN PASSWORD 'P@ssw0rd';
 
 GRANT scm TO admin;
 GRANT rman TO admin;
@@ -15,6 +18,9 @@ GRANT oozie TO admin;
 GRANT das TO admin;
 GRANT schemaregistry TO admin;
 GRANT smm TO admin;
+GRANT actmon TO admin;
+GRANT navaud TO admin;
+GRANT navmeta TO admin;
 
 CREATE USER rangeradmin WITH PASSWORD 'P@ssw0rd';
 GRANT rangeradmin TO admin;
